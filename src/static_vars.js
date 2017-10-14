533ec4bd4473d4cffbd5391fac7ed71b01b0c51f
Valder Gallo <valdergallo@gmail.com>
2017-10-14 08:41:59 -0300

    fix static vars

--- src/static_vars.js
+++ src/static_vars.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:8000/';
+// const BASE_URL = 'http://localhost:8000/';
+const BASE_URL = 'https://snitcher.herokuapp.com/';
 const API_LINK_URL = BASE_URL + 'api/pagelink/';
 const API_TRACK_URL = BASE_URL + 'api/tracker/';
 
